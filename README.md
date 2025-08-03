10d803ac1795f0b103b50484377da412e034acd8
Luca Foppiano <luca@foppiano.org>
2025-08-03 16:31:09 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
-# pdfalto-evaluation
+# Grobid text extraction metrics
+
+These metrics are provided for comparing the extracted output from pdfAlto with the Grobid structure. 
+The Grobid structure passes a set of models in cascade and  
+We create this metrics using the four corpora already used in the Grobid end to end evaluation benchmarks: PLOS_1000, PMC_sample_1943, eLife_984, and biorxiv-10k-test-2000.
+
+We calculate difference between the pdfalto text and the grobid text using character and token counting comparison.
+
+## Metrics
+
+Metrics are calculated as follows: 
+- The pdfalto text is extracted from pdfalto with a specific transformation schema illustrated [here](https://github.com/kermitt2/pdfalto/tree/master/schema).  
+- The grobid text is recusively extracted with a python XML parser
+- Both text are then stripped of their breaklines, whitespaces and they are assembled into two strings: 
+  - a version with spaces separating each words and symbol, used for the number of tokens comparison 
+  - a version without spaces, used for the character length comparison 
+- Two metrics are calculated for each version (character, tokens): 
+  - difference, computed as `length_pdfalto - lenght_grobid` which could be difference in characters or number of tokens 
+  - the percentage of difference, computed as (1 - (lenght/grobid / length_pdfalto)) * 100
+- The average values were produced by corpus and by process type
+
+### Preliminary discussion
+- Ligatures are preserved in pdfalto and converted into single characters by Grobid, therefore the Grobid character lenght might be larger for certain PDF documents
+- Footers and headers decoration are normally removed by Grobid and they are more frequent in PMC_sample_1943, PLOS_1000 and eLife_984, while preprint in biorxiv-10k-test-2000 are generally plain documents without headers/footers
+- Reference callout may be unwrapped by Grobid (`[9-15]` may become `[9][10][11][12][13][14][15]`) leading to larger character and token count
+- In the Grobid `standard` output, affiliation are usually duplicated in the author information, so depending on the number of authors the text in the Grobid `standard` output may be larger
+- Cover page, generally ignored by Grobid, may contain relevant amount of text for certain publishers
+- Grobid apply de-hypenisation, leading to a smaller increase in tokens count
+
+### 2025-03-08
+
+
+### 2025-03-07
+
+Diff: https://github.com/kermitt2/grobid/compare/8b9d113d665bc1bd64c3c38e4ca93a19a7426cb9...8ccde015d5b07f6a431826ab406d069222d8fd33
+
+#### Character length comparison
+
+
+| Process type                                  | PLOS_1000 | eLife_984 | biorxiv-10k-test-2000 | PMC_sample_1943 |
+|:----------------------------------------------|----------:|----------:|----------------------:|----------------:|
+| standard                                      |     11.82 |      9.64 |                  9.26 |            6.45 |
+| standard (paragraphs)                         |     11.81 |      9.63 |                  9.23 |            6.45 |
+| standard + collectDiscardedText               |      7.57 |      2.72 |                  2.92 |            3.75 |
+| standard + collectDiscardedText (paragraphs)  |      7.57 |      2.71 |                  2.89 |            3.75 |
+| light                                         |     12.45 |      6.85 |                  8.83 |            8.51 |
+| light (paragraphs)                            |     12.45 |      6.85 |                  8.80 |            8.51 |
+| light + collectDiscardedText                  |      3.18 |      3.33 |                  4.07 |            2.13 |
+| light + collectDiscardedText (paragraphs)     |      3.18 |      3.33 |                  4.04 |            2.13 |
+
+
+#### Token length comparison
+
+| Process type                                     | PLOS_1000 | eLife_984 |   biorxiv-10k-test-2000 | PMC_sample_1943 |
+|:-------------------------------------------------|----------:|----------:|------------------------:|----------------:|
+| standard                                         |      8.08 |      5.46 |                    7.44 |            3.63 |
+| standard (paragraphs)                            |      8.07 |      5.45 |                    7.37 |            3.63 |
+| standard + collectDiscardedText                  |      3.62 |     -2.36 |                    0.42 |            0.62 |
+| standard + collectDiscardedText (paragraphs)     |      3.61 |     -2.37 |                    0.35 |            0.62 |
+| light                                            |      8.93 |      2.74 |                    5.66 |            5.12 |
+| light (paragraphs)                               |      8.93 |      2.74 |                    5.59 |            5.12 |
+| light + collectDiscardedText                     |     -0.10 |     -0.51 |                    1.39 |            -0.7 |
+| light + collectDiscardedText (paragraphs)        |     -0.10 |     -0.51 |                    1.32 |            -0.7 |                                
+
+### 2025-02-01
+
+#### Character length comparison
+
+| Process type                                            | PLOS_1000 | eLife_984 | biorxiv-10k-test-2000 | PMC_sample_1943 (token) |
+|---------------------------------------------------------|-----------|-----------|-----------------------|-------------------------|
+| standard                                                | 11.27     | 9.69      | 9.33                  | 6.27                    |
+| standard (paragraphs)                                   | 11.84     | 9.72      | 9.48                  | 6.53                    |
+| standard + collect_discarded_text                       | 10.16     | 9.35      | 9.07                  | 5.76                    |   
+| standard + collect_discarded_text (paragraphs)          | 10.73     | 9.38      | 9.22                  | 6.02                    |   
+| article/light-ref                                       | 12.46     | 6.85      | 8.84                  | 8.52                    |
+| article/light-ref (paragraphs)                          | 12.46     | 6.85      | 8.79                  | 8.52                    |
+| article/light-ref + collect_discarded_text              | 3.18      | 3.32      | 4.08                  | 2.17                    | 
+| article/light-ref + collect_discarded_text (paragraphs) | 3.18      | 3.32      | 4.04                  | 2.17                    | 
+
+- `Standard` vs `standard + collectDiscardedText`, there is an increase about 1% in recovered text
+- `Standard` vs `article/light-ref`, there is a fluctuation in term of text which does not indicate any specific trend, article/light-ref may loose some information such as the abstract and other header related information that are not covered by the lightweight header model. 
+- `article/light-ref` vs `article/light-ref + collectDiscardedText` difference with pdfalto are decreasing as the text that is discarded by the `article/light-ref` header model is now recovered 
+
+
+#### Token length comparison
+
+| Process type                                             | PLOS_1000 | eLife_984 | biorxiv-10k-test-2000 | PMC_sample_1943 (token) |
+|----------------------------------------------------------|-----------|-----------|-----------------------|-------------------------|
+| standard                                                 | 7.62      | 5.55      | 7.63                  | 3.58                    |           
+| standard (paragraphs)                                    | 8.11      | 5.58      | 7.73                  | 3.75                    |           
+| standard + collect_discarded_text                        | 6.56      | 5.22      | 7.38                  | 3.09                    |          
+| standard + collect_discarded_text  (paragraphs)          | 7.05      | 5.24      | 7.48                  | 3.28                    |          
+| article/light-ref                                        | 8.94      | 2.74      | 5.66                  | 5.12                    |
+| article/light-ref (paragraphs)                           | 8.94      | 2.74      | 5.58                  | 5.13                    |
+| article/light-ref + collect_discarded_text               | -0.09     | -0.51     | 1.39                  | -0.72                   |           
+| article/light-ref + collect_discarded_text (paragraphs)  | -0.09     | -0.51     | 1.32                  | -0.72                   |           
+
+
+- `Standard` vs `standard + collectDiscardedText`, there is an increase about 0.5-1% in recovered text
+- `Standard` vs `article/light-ref`, there is a fluctuation in term of text which does not indicate any specific trend, article/light-ref may loose some information such as the abstract and other header related information that are not covered by the lightweight header model.
+- `article/light-ref` vs `article/light-ref + collectDiscardedText` difference with pdfalto are decreasing as the text that is discarded by the `article/light-ref` header model is now recovered
+- The negative numbers in the `article/light-ref + collect_discarded_text` can be due to dehypenisation applied by Grobid 
+
+
+## Notes
+
+- grobid.v3: 8b9d113d665bc1bd64c3c38e4ca93a19a7426cb9 (before fixing issue #1264 and bug with the figure not being reverted)
+- grobid.v4: https://github.com/kermitt2/grobid/pull/1264/commits/8ccde015d5b07f6a431826ab406d069222d8fd33 (after fixing collection of discarded text from the graphics aggregation)
+- grobid.v5: https://github.com/kermitt2/grobid/pull/1266
+- grobid.v6: https://github.com/kermitt2/grobid/pull/1268 
+
+
+Diff v3 to v4: https://github.com/kermitt2/grobid/compare/8b9d113d665bc1bd64c3c38e4ca93a19a7426cb9...8ccde015d5b07f6a431826ab406d069222d8fd33
---
+++ scripts/text_comparison.py
@@ -0,0 +1,256 @@
+import argparse
+import concurrent.futures
+import os
+
+import sacrebleu
+from rouge_score import rouge_scorer
+from tqdm import tqdm
+
+
+def process_file_pair(args):
+    file_id, file_A, file_B = args
+    if not os.path.exists(file_B):
+        return (file_id, None, file_B)
+
+    with open(file_A, "r") as f:
+        txt_pdfalto_A = f.read()
+    with open(file_B, "r") as f:
+        txt_pdfalto_B = f.read()
+
+    txt_pdfalto_A_spaces = ' '.join(txt_pdfalto_A.split())
+    txt_pdfalto_B_spaces = ' '.join(txt_pdfalto_B.split())
+    txt_pdfalto_A_spaces_len = len(txt_pdfalto_A_spaces)
+    txt_pdfalto_B_spaces_len = len(txt_pdfalto_B_spaces)
+    diff_chars_spaces = txt_pdfalto_A_spaces_len - txt_pdfalto_B_spaces_len
+
+    txt_pdfalto_A_no_spaces = ''.join(txt_pdfalto_A.split())
+    txt_pdfalto_B_no_spaces = ''.join(txt_pdfalto_B.split())
+    txt_pdfalto_A_len = len(txt_pdfalto_A_no_spaces)
+    txt_pdfalto_B_len = len(txt_pdfalto_B_no_spaces)
+    diff_chars_no_spaces = txt_pdfalto_A_len - txt_pdfalto_B_len
+
+    txt_pdfalto_A_token_len = len(txt_pdfalto_A_spaces.split())
+    txt_pdfalto_B_token_len = len(txt_pdfalto_B_spaces.split())
+    diff_tokens = txt_pdfalto_A_token_len - txt_pdfalto_B_token_len
+
+    # Use sacrebleu for faster BLEU
+    # bleu_score = sacrebleu.sentence_bleu(txt_pdfalto_A_spaces, [txt_pdfalto_B_spaces]).score / 100.0
+    # scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+    # rouge_scores = scorer.score(txt_pdfalto_B_spaces, txt_pdfalto_A_spaces)
+    # rouge_l_f1 = rouge_scores['rougeL'].fmeasure
+    return (file_id, {
+        'pdfalto_A': len(txt_pdfalto_A),
+        'pdfalto_A_tokens': txt_pdfalto_A_token_len,
+        'pdfalto_B': len(txt_pdfalto_B),
+        'pdfalto_B_tokens': txt_pdfalto_B_token_len,
+        'stats': {
+            'diff_chars_no_spaces': diff_chars_no_spaces,
+            'diff_chars_spaces': diff_chars_spaces,
+            'diff_tokens': diff_tokens,
+            # 'bleu': bleu_score,
+            # 'rougeL_f1': rouge_l_f1
+        }
+    }, None)
+
+
+def compute_run_statistics(input_pdfalto_A, input_pdfalto_B):
+    """
+    This method computes the statistics between two different runs,
+    searching for the common sub-directories and comparing each of them as separated
+    corpora
+    """
+    common_subdirs = extract_common_repositories(input_pdfalto_A, input_pdfalto_B)
+    print("Found the following corpus directories: ", common_subdirs)
+
+    documents = {}
+    for corpus in common_subdirs:
+        print(corpus)
+        file_pairs = []
+        for root, dirs, files in tqdm(os.walk(os.path.join(input_pdfalto_A, corpus)), desc="Reading files..."):
+            for file in files:
+                if not file.endswith(".txt"):
+                    continue
+                # Use only string types for all path operations
+                file_path_A = os.path.join(root, file)
+                rel_dir = os.path.relpath(root, os.path.join(input_pdfalto_A, corpus))
+                if rel_dir == ".":
+                    file_id = file.replace(".txt", "")
+                else:
+                    file_id = os.path.join(rel_dir, file).replace(".txt", "")
+                file_A = file_path_A
+                file_B = os.path.join(input_pdfalto_B, corpus, rel_dir, file)
+                file_pairs.append((file_id, file_A, file_B))
+
+        skipped = []
+        processed = {}
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = list(tqdm(executor.map(process_file_pair, file_pairs), total=len(file_pairs), desc="Computing differences..."))
+
+        for file_id, result, skip_path in results:
+            if result is None:
+                skipped.append((file_id, skip_path))
+            else:
+                processed[file_id] = result
+        documents[corpus] = {
+            'processed': processed,
+            'skipped': skipped,
+            'pdfalto_A': input_pdfalto_A,
+            'pdfalto_B': input_pdfalto_B,
+            'average': {
+                'pdfalto_A': 0,
+                'pdfalto_B': 0,
+                'diff': 0,
+                'perc': 0
+            }
+        }
+        print(f"Processed documents {len(processed)}")
+        print(f"Skipped documents {len(skipped)}")
+    return documents
+
+
+def extract_common_repositories(input_pdfalto, input_grobid):
+    # Find the subdirectories in the input corpora
+    subdirs_pdfalto = [x for x in os.listdir(input_pdfalto)]
+    print(f"Found the following corpus directories for PDFAlto A: {subdirs_pdfalto}")
+    subdirs_grobid = [x for x in os.listdir(input_grobid)]
+    print(f"Found the following corpus directories for PDFAlto B: {subdirs_grobid}")
+    # find the common subdirectories
+    common_subdirs = [x for x in subdirs_grobid if x in subdirs_pdfalto]
+    return common_subdirs
+
+
+def compute_averages(documents_):
+    for process_type, process in documents_.items():
+        for corpus, documents in process.items():
+            total_pdfalto_A = sum([d['pdfalto_A'] for d in documents['processed'].values()])
+            total_pdfalto_B = sum([d['pdfalto_B'] for d in documents['processed'].values()])
+            total_diff_chars_no_spaces = sum(
+                [d['stats']['diff_chars_no_spaces'] for d in documents['processed'].values()])
+            total_diff_chars_spaces = sum([d['stats']['diff_chars_spaces'] for d in documents['processed'].values()])
+            matching_documents_chars_no_spaces = sum(
+                [d['stats']['diff_chars_no_spaces'] == 0 for d in documents['processed'].values()])
+            matching_documents_chars_spaces = sum(
+                [d['stats']['diff_chars_spaces'] == 0 for d in documents['processed'].values()])
+            total_diff_tokens = sum([d['stats']['diff_tokens'] for d in documents['processed'].values()])
+            matching_documents_tokens = sum([d['stats']['diff_tokens'] == 0 for d in documents['processed'].values()])
+
+            documents['average']['pdfalto_A'] = total_pdfalto_A / len(documents['processed'])
+            documents['average']['pdfalto_B'] = total_pdfalto_B / len(documents['processed'])
+            documents['average']['diff_chars_no_spaces'] = total_diff_chars_no_spaces / len(documents['processed'])
+            documents['average']['diff_chars_spaces'] = total_diff_chars_spaces / len(documents['processed'])
+            documents['average']['diff_tokens'] = total_diff_tokens / len(documents['processed'])
+            documents['average']['matching_documents_chars_spaces'] = matching_documents_chars_spaces
+            documents['average']['matching_documents_chars_no_spaces'] = matching_documents_chars_no_spaces
+            documents['average']['matching_documents_tokens'] = matching_documents_tokens
+
+    return documents_
+
+
+def compute_matching_elements(documents_output_with_average):
+    columns = ["Chars/Tokens"]
+    aggregated_data = []
+    for process_type, process in documents_output_with_average.items():
+        process_data = ["Chars (no spaces)"]
+        for corpus, documents in process.items():
+            if corpus not in columns:
+                columns.append(corpus)
+            process_data.append(documents['average']['matching_documents_chars_no_spaces'])
+        aggregated_data.append(process_data)
+        process_data = ["Chars (spaces)"]
+        for corpus, documents in process.items():
+            if corpus not in columns:
+                columns.append(corpus)
+            process_data.append(documents['average']['matching_documents_chars_spaces'])
+        aggregated_data.append(process_data)
+        process_data = ["Tokens"]
+        for corpus, documents in process.items():
+            process_data.append(documents['average']['matching_documents_tokens'])
+        aggregated_data.append(process_data)
+
+    return aggregated_data
+
+
+def compute_differences(document_output_with_average):
+    columns = ["Process type"]
+    aggregated_data = []
+    for process_type, process in documents_output_with_average.items():
+        process_data = ["Chars (no spaces)"]
+        for corpus, documents in process.items():
+            if corpus not in columns:
+                columns.append(corpus)
+            process_data.append(documents['average']['diff_chars_no_spaces'])
+        aggregated_data.append(process_data)
+        process_data = ["Chars (spaces)"]
+        for corpus, documents in process.items():
+            if corpus not in columns:
+                columns.append(corpus)
+            process_data.append(documents['average']['diff_chars_spaces'])
+        aggregated_data.append(process_data)
+        process_data = ["Tokens"]
+        for corpus, documents in process.items():
+            process_data.append(documents['average']['diff_tokens'])
+        aggregated_data.append(process_data)
+
+    return aggregated_data
+
+
+def find_greatest_differences(documents_output):
+    for process_type, process in documents_output.items():
+        print(process_type)
+        for corpus, documents in process.items():
+            print(f"\t{corpus}")
+            sorted_by_diff = sorted(
+                documents['processed'].items(),
+                key=lambda x: x[1]['stats']['diff_chars_no_spaces']
+            )
+
+            sorted_by_diff_tokens = sorted(
+                documents['processed'].items(),
+                key=lambda x: x[1]['stats']['diff_tokens']
+            )
+
+            output_neg = [f"\n\t\t\t -{item[0]}: {item[1]['stats']['diff_chars_no_spaces']}" for item in
+                          sorted_by_diff[:2]]
+            print("".join(output_neg))
+            output_pos = [f"\n\t\t\t -{item[0]}: {item[1]['stats']['diff_chars_no_spaces']}" for item in
+                          sorted_by_diff[-2:]]
+            print("".join(output_pos))
+            # print(f"Files to check:\n {[item[0] for item in sorted_by_diff_tokens[:2]]}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compare two PDFAlto runs.")
+    parser.add_argument('--run1', required=True, help='Path to first PDFAlto output directory')
+    parser.add_argument('--run2', required=True, help='Path to second PDFAlto output directory')
+    args = parser.parse_args()
+
+    pdfalto_A = args.run1
+    pdfalto_B = args.run2
+
+    documents_output = {}
+    documents_output["standard"] = compute_run_statistics(pdfalto_A, pdfalto_B)
+    documents_output_with_average = compute_averages(documents_output)
+    matching = compute_matching_elements(documents_output_with_average)
+    diff = compute_differences(documents_output_with_average)
+
+
+    def print_markdown_table(title, data):
+        print(f"\n## {title}\n")
+        # Assume first row contains headers
+        headers = [str(h) for h in data[0]]
+        print("| " + " | ".join(headers) + " |")
+        print("|" + "---|" * len(headers))
+        for row in data[1:]:
+            print("| " + " | ".join(str(cell) for cell in row) + " |")
+
+
+    # Prepare matching and diff tables with headers
+    matching_table = [
+                         ["Metric"] + list(documents_output_with_average["standard"].keys())
+                     ] + matching
+    diff_table = [
+                     ["Metric"] + list(documents_output_with_average["standard"].keys())
+                 ] + diff
+
+    print_markdown_table("Matching Documents", matching_table)
+    print_markdown_table("Differences (Averages)", diff_table)
